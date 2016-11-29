f1a5b20d9efa9b641e0e69a284e899cb76543dd8
Nigel Finley <nfinleymusic@gmail.com>
2016-11-28 18:02:01 -0600

    updated index

--- models/index.js
+++ models/index.js
@@ -14,35 +14,41 @@ var db        = {};
 //   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 // }
 
+if (process.env.JAWSDB_URL) {
+    var sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+    var sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
 //==== NEW TEST SCRIPTS TO GET DB WORKING ON HEROKU =======
 
-if (!global.hasOwnProperty('db')) {
-    var Sequelize = require('sequelize')
-        , sequelize = null
-
-    if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
-        // the application is executed on Heroku ... use the postgres database
-        sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
-            dialect:  'postgres',
-            protocol: 'postgres',
-            port:     match[4],
-            host:     match[3],
-            logging:  true //false
-        })
-    } else {
-        // the application is executed on the local machine ... use mysql
-        sequelize = new Sequelize('example-app-db', 'root', null)
-    }
-
-    global.db = {
-        Sequelize: Sequelize,
-        sequelize: sequelize,
-        User:      sequelize.import(__dirname + '/user'),
-        Todo:      sequelize.import(__dirname + '/todo')
-        // add your other models here
-    }
-
-}
+// if (!global.hasOwnProperty('db')) {
+//     var Sequelize = require('sequelize')
+//         , sequelize = null
+//
+//     if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
+//         // the application is executed on Heroku ... use the postgres database
+//         sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
+//             dialect:  'postgres',
+//             protocol: 'postgres',
+//             port:     match[4],
+//             host:     match[3],
+//             logging:  true //false
+//         })
+//     } else {
+//         // the application is executed on the local machine ... use mysql
+//         sequelize = new Sequelize('example-app-db', 'root', null)
+//     }
+//
+//     global.db = {
+//         Sequelize: Sequelize,
+//         sequelize: sequelize,
+//         User:      sequelize.import(__dirname + '/user')
+//         Todo:      sequelize.import(__dirname + '/todo')
+//         // add your other models here
+//     }
+//
+// }
 
 
 
