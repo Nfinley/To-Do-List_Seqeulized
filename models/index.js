fa230fa1c48d971b76d09ae72fbce04bf82284a1
Nigel Finley <nfinleymusic@gmail.com>
2016-11-28 18:10:41 -0600

    updated index file with new env variable

--- models/index.js
+++ models/index.js
@@ -14,7 +14,7 @@ var db        = {};
 //   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 // }
 
-if (process.env.JAWSDB_URL) {
+if (config.use_env_variable) {
     var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
     var sequelize = new Sequelize(config.database, config.username, config.password, config);
